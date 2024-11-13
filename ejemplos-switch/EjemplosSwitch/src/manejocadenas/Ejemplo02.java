985d4f9867095c34ebd2c6f7c185f9811d76fd2c
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-11-13 12:42:00 -0500

    Cambios

--- ejemplos-switch/EjemplosSwitch/src/manejocadenas/Ejemplo02.java
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/Ejemplo02.java
@@ -16,7 +16,7 @@ public class Ejemplo02 {
         
         pais = "Ecuador";
         //      0123456
-        char letra = pais.charAt(0);
+        char letra = pais.charAt(4);
         System.out.printf("%s\n", letra);
         
     }
